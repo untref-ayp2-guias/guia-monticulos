a1c09d24e9a1f077d730401e0f1ccff3dda86198
Leandro Doctors <ldoctors@untref.edu.ar>
2023-05-16 17:48:10 -0300

    Agregar implementaciones

---
+++ src/monticulo/monticulo_min.go
@@ -0,0 +1,77 @@
+package monticulo
+
+import (
+	"fmt"
+
+	"golang.org/x/exp/constraints"
+)
+
+// Monticulo es una estructura de datos que permite almacenar elementos
+// de forma ordenada. En este caso, se implementa un montículo mínimo.
+type MonticuloMin[T constraints.Ordered] struct {
+	// Elementos del montículo
+	elementos []T
+}
+
+// NewMonticulo crea un nuevo montículo vacío.
+func NewMonticuloMin[T constraints.Ordered]() *MonticuloMin[T] {
+	return &MonticuloMin[T]{}
+}
+
+// Len devuelve el número de elementos del montículo.
+func (m *MonticuloMin[T]) Len() int {
+	return len(m.elementos)
+}
+
+// Insertar inserta un nuevo elemento en el montículo.
+func (m *MonticuloMin[T]) Insertar(elemento T) {
+	m.elementos = append(m.elementos, elemento)
+	m.upHeap(len(m.elementos) - 1)
+}
+
+// upHeap reordena el montículo hacia arriba.
+// Costo: O(log n)
+func (m *MonticuloMin[T]) upHeap(i int) {
+	for i > 0 {
+		padre := (i - 1) / 2
+		if m.elementos[i] >= m.elementos[padre] {
+			break
+		}
+		m.elementos[i], m.elementos[padre] = m.elementos[padre], m.elementos[i]
+		i = padre
+	}
+}
+
+// BorrarMin extrae el elemento mínimo del montículo.
+func (m *MonticuloMin[T]) BorrarMin() (T, error) {
+	var elemento T
+	if m.Len() == 0 {
+		return elemento, fmt.Errorf("Montículo vacío")
+	}
+	elemento = m.elementos[0]
+	m.elementos[0] = m.elementos[m.Len()-1]
+	m.elementos = m.elementos[:m.Len()-1]
+	m.downHeap(0)
+	return elemento, nil
+}
+
+// downHeap reordena el montículo hacia abajo.
+// Costo: O(log n)
+func (m *MonticuloMin[T]) downHeap(i int) {
+	for {
+		izq := 2*i + 1
+		der := 2*i + 2
+		if izq >= m.Len() {
+			break
+		}
+		min := izq
+		if der < m.Len() && m.elementos[der] < m.elementos[izq] {
+			min = der
+		}
+		if m.elementos[i] <= m.elementos[min] {
+			break
+		}
+		m.elementos[i], m.elementos[min] = m.elementos[min], m.elementos[i]
+		i = min
+	}
+}
